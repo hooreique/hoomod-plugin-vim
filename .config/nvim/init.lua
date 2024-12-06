88d50572554bf5d1b26604b80b671e173a6c1d52
Song Jeyeong <46372718+hooreique@users.noreply.github.com>
2024-12-06 14:06:44 +0900

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,75 +1,140 @@
--- Environment settings
-vim.env.LANG = 'en_US'
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.wrap = true
+vim.opt.number = true
+vim.opt.timeout = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.undofile = true
+vim.opt.scrolloff = 8
+vim.opt.updatetime = 250
+vim.opt.hlsearch = false
 
--- General settings
-local options = {
-    errorbells = false,
-    visualbell = true,
-    ignorecase = true,
-    smartcase = true,
-    number = true,
-    autoindent = true,
-    expandtab = true,
-    tabstop = 4,
-    shiftwidth = 4,
-    scrolloff = 10,
-}
-for k, v in pairs(options) do
-    vim.opt[k] = v
-end
+local modes = {'n', 'v', 'o'}
 
--- Helper function for key mappings
-local function map(mode, lhs, rhs, opts)
-    opts = opts or { noremap = true, silent = true }
-    vim.keymap.set(mode, lhs, rhs, opts)
-end
+local keys = {
+  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+  'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+  'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
 
--- Disable specific keys
-local function disable_keys(keys)
-    for _, key in ipairs(keys) do
-        map('', key, '<Nop>')
-    end
-end
+  '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
 
-disable_keys {
-    '<Up>', '<Down>', '<Left>', '<Right>', '<F1>', '<F2>', '<F3>', '<F4>',
-    '<F5>', '<F6>', '<F7>', '<F8>', '<F9>', '<F10>', '<F11>', '<F12>', '<Space>',
-    '`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+',
-    '-', '=', '[', ']', '{', '}', '\\', '|', ';', '\'', '"', ',', '.',
-    '/', '<', '>', '?', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
-    'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-    'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+  '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+',
+  '=', '[', ']', '{', '}', '|', '\\', ';', "'", '"', ',', '.',
+  '<', '>', '/', '?', '`', '~', -- ':',
+
+  '<F1>', '<F2>', '<F3>', '<F4>', '<F5>', '<F6>', '<F7>', '<F8>',
+  '<F9>', '<F10>', '<F11>', '<F12>',
+
+  '<Left>', '<Right>', '<Up>', '<Down>', '<Home>', '<End>', '<PageUp>', '<PageDown>',
+
+  '<Space>', '<Tab>', '<CR>', '<BS>', '<Delete>', '<Insert>', -- '<Esc>',
 }
 
--- Key mappings
-local normal_mappings = {
-    ['u'] = 'gk', ['e'] = 'gj', ['n'] = 'h', ['i'] = 'l',
-    ['U'] = '?\\S\\s*\\n\\s*\\n<CR>0', ['E'] = '/\\S\\s*\\n\\s*\\n<CR>/\\S<CR>k0',
-    ['N'] = 'ge', ['I'] = 'w', ['h'] = 'g0', ['o'] = 'g$',
-    ['H'] = 'gg', ['O'] = 'G$', ['y'] = '``', ['f'] = '3<C-Y>',
-    ['s'] = '3<C-E>', ['F'] = '<C-U>', ['S'] = '<C-D>', ['<Space>'] = 'zz',
-    ['d'] = '"zdd', ['v'] = '"zp', ['D'] = '"+dd', ['V'] = '"+p',
-    ['t'] = 'a', ['r'] = 'i', ['T'] = 'R', ['R'] = 'O', ['<CR>'] = 'A<CR>',
-    ['g'] = 'v',
-}
+local o = { noremap = true, nowait = true, silent = true }
 
-for k, v in pairs(normal_mappings) do
-    map('n', k, v)
+for _, key in ipairs(keys) do
+  vim.keymap.set(modes, key, '<Nop>', o)
 end
 
-local visual_mappings = {
-    ['u'] = 'gk', ['e'] = 'gj', ['n'] = 'h', ['i'] = 'l',
-    ['U'] = '{k$', ['E'] = '}j^', ['N'] = 'ge', ['I'] = 'w',
-    ['h'] = 'g0', ['o'] = 'g$', ['H'] = 'gg', ['O'] = 'G$',
-    ['y'] = '``', ['f'] = '3<C-Y>', ['s'] = '3<C-E>', ['F'] = '<C-U>',
-    ['S'] = '<C-D>', ['<Space>'] = 'zz', ['d'] = '"zd', ['c'] = '"zy',
-    ['v'] = '"zp', ['D'] = '"+d', ['C'] = '"+y', ['V'] = '"+p',
+local nv_maps = {
+  { '<Up>'    , 'gk' },
+  { '<Down>'  , 'gj' },
+  { '<Left>'  , 'h'  },
+  { '<Right>' , 'l'  },
+
+  { 'u' , 'gk'                           },
+  { 'e' , 'gj'                           },
+  { 'n' , 'h'                            },
+  { 'i' , 'l'                            },
+  { 'U' , [[?\S\s*\n\s*\n<CR>0]]         },
+  { 'E' , [[/\S\s*\n\s*\n<CR>/\S<CR>k0]] },
+  { 'N' , 'ge'                           },
+  { 'I' , 'w'                            },
+
+  { '<Home>' , 'g0' },
+  { '<End>'  , 'g$' },
+
+  { 'h' , 'g0' },
+  { 'o' , 'g$' },
+  { 'H' , 'gg' },
+  { 'O' , 'G$' },
+
+  { 'y' , '``' },
+
+  { 'f' , '3<C-Y>' },
+  { 's' , '3<C-E>' },
+  { 'F' , '<C-U>'  },
+  { 'S' , '<C-D>'  },
+
+  { '<Space>' , 'zz' },
+
+  { 'c' , '"zy' },
+  { 'v' , '"zp' },
+  { 'C' , '"+y' },
+  { 'V' , '"+p' },
+
+  { 'x' , 'x' },
 }
 
-for k, v in pairs(visual_mappings) do
-    map('v', k, v)
+for _, map in ipairs(nv_maps) do
+  vim.keymap.set({'n', 'v'}, map[1], map[2], o)
 end
 
--- Custom message
+local v_maps = {
+  { 'd' , '"zd' },
+  { 'D' , '"+d' },
+
+  { '<BS>'  , 'd' },
+  { '<Del>' , 'd' },
+}
+
+for _, map in ipairs(v_maps) do
+  vim.keymap.set('v', map[1], map[2], o)
+end
+
+local n_maps = {
+  { 'd' , '"zdd' },
+  { 'D' , '"+dd' },
+
+  { '<BS>'  , 'dhi' },
+  { '<Del>' , 'xi'  },
+
+  { 't'    , 'a'     },
+  { 'r'    , 'i'     },
+  { 'T'    , 'R'     },
+  { 'R'    , 'O'     },
+  { '<CR>' , 'A<CR>' },
+  { 'g'    , 'v'     },
+
+  { ';' , 'A;<ESC>'   },
+  { '{' , 'A<Space>{' },
+
+  { 'b' , 'gd' },
+  { 'B' , 'gD' },
+
+  { '/' , '/' },
+  { '?' , '?' },
+  { '.' , 'n' },
+  { '>' , 'N' },
+
+  { 'k' , [[:%s/\<<C-R><C-W>\>//g<Left><Left>]] },
+
+  { '<Tab>' , '[{=%' },
+
+  { 'z' , 'u'     },
+  { 'Z' , '<C-R>' },
+
+  { 'q' , 'qy' },
+  { 'Q' , 'q'  },
+  { 'p' , '@y' },
+}
+
+for _, map in ipairs(n_maps) do
+  vim.keymap.set('n', map[1], map[2], o)
+end
+
 vim.notify('Not sure about hoomod? Type :q! to exit.', vim.log.levels.INFO)
